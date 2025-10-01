2c742ca13b16d27c86783e4ee14fb978a984273b
Bipul <devbtes06@gmail.com>
2025-10-01 12:59:34 +0600

    Update page.tsx

--- src/app/exam/page.tsx
+++ src/app/exam/page.tsx
@@ -5,9 +5,9 @@ export default function page() {
   return (
     <div>
         <h1 className="text-4xl text-center">This is a exam site</h1>
-        <Link className="mt-10" href="/start">
+        
         <Image src={Arr} alt="left-arrow" width={35} height={35}/>
-        </Link>
+        
         <div className="classes justify-center align-center grid gap-10 mt-60">
           <button><Link href ="/genaral">Genaral</Link></button>
     <button><Link href ="/vocational">Vocational</Link></button>
